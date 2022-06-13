73ae78ad551c9b1e38c5b8ab70dd088b7b712417
Maxiee <maxieewong@gmail.com>
2022-06-14 01:07:14 +0800

    sync success

--- src/processes/gclient_sync.py
+++ src/processes/gclient_sync.py
@@ -7,4 +7,8 @@ class ProcessGClientSync:
     def run(self) -> bool:
         subprocess.Popen(['gclient', 'sync'], cwd=Path().joinpath('engine'))
 
+        # https://stackoverflow.com/questions/13744473/command-line-execution-in-different-folder
+        # when sync success, will show:
+        # Syncing projects: 100% (126/126), done.
+
         return True
