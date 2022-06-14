149019203e294b60183f4a5ba5dc0e4d35f020cd
Maxiee <maxieewong@gmail.com>
2022-06-14 22:00:15 +0800

    code sync

--- src/main.py
+++ src/main.py
@@ -5,16 +5,13 @@ from processes.dependencies import ProcessDependenciesCheck
 from processes.gclient_init import ProcessInitGClient
 from processes.gclient_sync import ProcessGClientSync
 
-
-def print_error():
-    print('compile failed, please review the exception first.')
-    exit(1)
-
-
 if __name__ == '__main__':
     # check dependencies
     if ProcessDependenciesCheck().run() \
             and ProcessInitGClient().run() \
             and ProcessGClientSync().run():
         print('success!')
-    print_error()
+        exit(0)
+
+    print('compile failed, please review the exception first.')
+    exit(1)
--- src/processes/gclient_sync.py
+++ src/processes/gclient_sync.py
@@ -5,8 +5,9 @@ from pathlib import Path
 class ProcessGClientSync:
 
     def run(self) -> bool:
-        subprocess.Popen(['gclient', 'sync'], cwd=Path().joinpath('engine'))
-
+        p = subprocess.Popen(['gclient', 'sync'],
+                             cwd=Path().joinpath('engine'))
+        p.wait()
         # https://stackoverflow.com/questions/13744473/command-line-execution-in-different-folder
         # when sync success, will show:
         # Syncing projects: 100% (126/126), done.
